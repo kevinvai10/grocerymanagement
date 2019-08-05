eece683bc8b355ec29057a67ed29d6dfdb426547
kevinvai10 <keancaba@gmail.com>
2019-08-04 20:59:59 -0700

    addded post method refactor

--- src/components/ProductAdd/ProductAdd.js
+++ src/components/ProductAdd/ProductAdd.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import {addProduct} from '../../utils/apicalls';
 import 'tachyons';
-const url = "http://localhost:3002/addproduct"
 
 class ProductAdd extends Component{
     constructor(){
@@ -39,28 +39,14 @@ class ProductAdd extends Component{
     handleSubmit(event){
         event.preventDefault();
 
-        const data = {
+        const newProduct = {
                 name: this.state.product_name,
                 price: this.state.product_price,
                 store: this.state.product_store,
                 category: this.state.product_category,
         }
-        console.log('data: ' , data)
-        fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, cors, *same-origin
-            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json',
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow', // manual, *follow, error
-            referrer: 'no-referrer', // no-referrer, *client
-            body: JSON.stringify(data), // body data type must match "Content-Type" header
-        })
-        .then(response => response.json())
-        .then(response => {
+
+        addProduct(newProduct).then(response => {
             alert("product added")
             this.setState({
                 product_name: "",
--- src/utils/apicalls.js
+++ src/utils/apicalls.js
@@ -1,10 +1,21 @@
-const baseUrl = "http://localhost:3002/products/";
+const getProductsUrl = "http://localhost:3002/products/";
+const addProductUrl = "http://localhost:3002/addproduct";
+//returns array of products
+const getProducts = () => fetch(getProductsUrl).then(response => response.json());
 
-const getProducts = () => {
-    console.log('url: ', baseUrl);
-    return fetch(baseUrl).then(response => response.json())
-}
+//adds product to db
+const addProduct = (data) => {
+    return fetch(addProductUrl , {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+    .then(response => response.json())
+} 
 
 export {
     getProducts,
+    addProduct
 }
