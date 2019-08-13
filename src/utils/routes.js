2fe2ea788c46ed76fed3c018bcf1b49de81312db
kevinvai10 <keancaba@gmail.com>
2019-08-12 19:38:16 -0700

    deploy

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,5 +1,20 @@
 const API_KEY ="ab533dea159a45eaaf550aa99f6d445d";
-const getProductsUrl = "http://localhost:3002/products";
+//server settings
+const getProductsUrl = "https://hidden-falls-24272.herokuapp.com/products";
+const getCategoriesUrl = "https://hidden-falls-24272.herokuapp.com/categories";
+const getStoresUrl = "https://hidden-falls-24272.herokuapp.com/stores";
+const addProductUrl = "https://hidden-falls-24272.herokuapp.com/addproduct";
+const registerUrl = "https://hidden-falls-24272.herokuapp.com/register";
+const signinUrl = "https://hidden-falls-24272.herokuapp.com/signin";
+const addCategoryUrl = "https://hidden-falls-24272.herokuapp.com/addcategory";
+const addStoreUrl = "https://hidden-falls-24272.herokuapp.com/addstore";
+const editProductUrl = "https://hidden-falls-24272.herokuapp.com/edit";
+const deleteProductUrl = "https://hidden-falls-24272.herokuapp.com/delete";
+const getRecipeByIngredientUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + API_KEY + "&ingredients=";
+const getRandomFoodTriviaUrl = "https://api.spoonacular.com/food/trivia/random?apiKey=" + API_KEY;
+
+//local settings
+/*const getProductsUrl = "http://localhost:3002/products";
 const getCategoriesUrl = "http://localhost:3002/categories";
 const getStoresUrl = "http://localhost:3002/stores";
 const addProductUrl = "http://localhost:3002/addproduct";
@@ -11,7 +26,7 @@ const editProductUrl = "http://localhost:3002/edit";
 const deleteProductUrl = "http://localhost:3002/delete";
 const getRecipeByIngredientUrl = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + API_KEY + "&ingredients=";
 const getRandomFoodTriviaUrl = "https://api.spoonacular.com/food/trivia/random?apiKey=" + API_KEY;
-
+*/
 export {
     getProductsUrl,
     getCategoriesUrl,
